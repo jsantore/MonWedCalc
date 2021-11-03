9534914dbc393e1612f269d698bc3eb3562140ef
jsantore <jsantore@bridgew.edu>
2021-11-03 15:03:58 -0400

    add and equals work - */ and - work in progress

--- src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
+++ src/main/java/bsu/comp152/monwedcalc/CalcWindow.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class CalcWindow {
+    private long firstNumber;
+    private OperatorType operation;
     @FXML
     private TextField numberField;
 
@@ -20,6 +22,28 @@ public class CalcWindow {
     @FXML
     public void clearButtonPressed(){
         numberField.clear();
+    }
+
+    @FXML
+    public void equalPressed(){
+        var secondNumberText = numberField.getText();
+        var secondNumber = Long.parseLong(secondNumberText);
+        var result = 0.0;
+        switch (operation){
+            case Add -> result = secondNumber+firstNumber;
+            case Multiply -> result = firstNumber * secondNumber;
+            case Subtract -> result = firstNumber - secondNumber;
+            case Divide -> result = (double)firstNumber / secondNumber;
+        }
+        numberField.setText(String.valueOf(result));
+    }
+
+    @FXML
+    public void plusPressed(){
+         var numberAsText = numberField.getText();
+         firstNumber = Long.parseLong(numberAsText);
+         numberField.setText("");
+         operation = OperatorType.Add;
     }
 
 }
